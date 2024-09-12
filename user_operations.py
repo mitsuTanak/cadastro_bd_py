43ca85d53836cf642dfac6e4b442791df77e8542
Mitsuaki Tanaka <130664643+mitsuTanak@users.noreply.github.com>
2024-09-12 12:07:01 -0300

    correções

--- user_operations.py
+++ user_operations.py
@@ -1,17 +1,17 @@
 from tkinter import messagebox
 
-# Inicializa a classe com referencia ao banco de dados e á interface do usuário
+# Inicializa a classe com referências ao banco de dados e à interface do usuário.
 class UserOperations:
     def __init__(self, db, ui):
         self.db = db
         self.ui = ui
 
-    # cadastrar um novo usuário no banco e dados
+    # Cadastra um novo usuário no banco de dados
     def cadastrar(self):
         nome = self.ui.nome_entry.get()
         if nome:
             self.db.insert_user(nome)
-            messagebox.showinfo("Sucesso", "Usuario cadastrado com sucesso!")
+            messagebox.showinfo("Sucesso", "Usuário cadastrado com sucesso!")
             self.ui.nome_entry.delete(0, 'end')
             self.ui.carregar_dados()
         else:
@@ -20,10 +20,11 @@ class UserOperations:
     # Atualiza as informações de um usuário existente
     def atualizar_usuario(self):
         if self.ui.selected_user:
-            novo_nome  = self.ui.nome_entry.get()
+            novo_nome = self.ui.nome_entry.get()
             if novo_nome:
                 self.db.update_user(self.ui.selected_user[0], novo_nome)
-                messagebox.showinfo("Sucesso", "Usuario atualizado com sucesso!")
+                messagebox.showinfo("Sucesso", "Usuário atualizado com sucesso!")
+                self.ui.carregar_dados()
                 self.ui.nome_entry.delete(0, 'end')
                 self.ui.selected_user = None
             else:
@@ -31,13 +32,12 @@ class UserOperations:
         else:
             messagebox.showerror("Erro", "Por favor, selecione um usuário para atualizar.")
 
-    # Excluir um usuário do banco de dados
-    def atualizar_usuario(self):
+    # Exclui um usuário do banco de dados
+    def excluir_usuario(self):
         if self.ui.selected_user:
-
-            if messagebox.askyesno("Comfirmar", "Tem certeza que deseja excluir este usuario?"):
+            if messagebox.askyesno("Confirmar", "Tem certeza que deseja excluir este usuário?"):
                 self.db.delete_user(self.ui.selected_user[0])
-                messagebox.showinfo("Sucesso", "Usuario excluido com sucesso!")
+                messagebox.showinfo("Sucesso", "Usuário excluído com sucesso!")
                 self.ui.carregar_dados()
                 self.ui.nome_entry.delete(0, 'end')
                 self.ui.selected_user = None
